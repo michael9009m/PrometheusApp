d7f975f7507fc76ed515c1b1a0e58ea9d9da16ca
Michael Martinez <Michael.martinez1@calbaptist.edu>
2024-03-11 16:43:14 -0700

    Initial Commit

---
+++ Prometheus/PrometheusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrometheusApp.swift
+//  Prometheus
+//
+//  Created by Michael Martinez on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PrometheusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
