4ecfc543098e21a12760669b9a4ee5bec71baeb3
Michael Martinez <Michael.martinez1@calbaptist.edu>
2024-03-19 22:53:49 -0700

    Functional Firebase Authentication

---
+++ Prometheus/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Prometheus
+//
+//  Created by Michael Martinez on 3/13/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
